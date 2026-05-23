1ea9ba3ffa8cb4fd08a426c9f870991e3c313c07
almarazmendozajhonluis-rgb <almarazmendozajhonluis@gmail.com>
2026-05-22 22:00:20 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,34 +2,52 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
-
 migrate = Migrate()
-
 login_manager = LoginManager()
+
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'clinica'
+    # SECRET KEY
+    app.config['SECRET_KEY'] = os.environ.get(
+        'SECRET_KEY',
+        'clinica'
+    )
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinica.db'
+    # BASE DE DATOS
+    database_url = os.environ.get('DATABASE_URL')
+
+    if database_url:
+        # Compatibilidad PostgreSQL Render
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace(
+                "postgres://",
+                "postgresql://",
+                1
+            )
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    else:
+        # Local (tu PC)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinica.db'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
     migrate.init_app(app, db)
 
     login_manager.init_app(app)
-
     login_manager.login_view = 'auth.login'
 
     from app.models.usuario import Usuario
 
     @login_manager.user_loader
     def load_user(user_id):
-
         return Usuario.query.get(int(user_id))
 
     # BLUEPRINTS
